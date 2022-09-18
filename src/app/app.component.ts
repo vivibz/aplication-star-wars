aa4addb7d76c2dcfe829186020f10a15d1badff0
Viviane Matias <vivianematiasbz00@gmail.com>
2022-09-18 16:44:16 -0300

    configuracao do ambiente inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {   // implements OnInit
   title = 'aplication-star-wars';
+  // titule : string = ''
+
+  // constructor(private _httpClient: HttpClient) {
+
+  // }
+  // ngOnInit(): void {
+  //   this._httpClient.get("https://swapi.dev/documentation")
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
