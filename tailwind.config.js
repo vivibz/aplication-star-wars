19b87b855a35e47cf9d80aea668ed2d4e8f97a51
Viviane Matias <vivianematiasbz00@gmail.com>
2022-12-19 17:55:11 -0300

    create the styles one cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/app/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '96': '53rem',
+      }
+    },
   },
   plugins: [],
 }
